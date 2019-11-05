f22312ef3b9c71e643d4b53a26ba9b8e21b45e48
陈志远 <chenzy01@eastcom-sw.com>
2019-11-05 10:27:56 +0800

    :bug: 加锁解决非线程安全问题

--- src/main/java/chapter01/section02/thread_1_2_3/pro_2_t4/MyThread.java
+++ src/main/java/chapter01/section02/thread_1_2_3/pro_2_t4/MyThread.java
@@ -12,11 +12,12 @@ public class MyThread extends Thread{
     private int count=5;
 
     @Override
-    public void run() {
-        //synchronized public void run() {
+    //public void run() {
+    synchronized public void run() {
         super.run();
         count--;
         System.out.println(" 由 "+this.currentThread().getName()+" 计算，count="+count);
+
     }
 
 }
