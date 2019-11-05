175bd4e7961715afb7bbc585fc7eecaf9f1865c6
陈志远 <chenzy01@eastcom-sw.com>
2019-11-05 10:43:56 +0800

    :sparkles: i-- 和 system.out.println 配合使用会出现非线程安全问题，加锁可解决这个问题

--- src/main/java/chapter01/section02/thread_1_2_4/pro_1_sameNum/MyThread.java
+++ src/main/java/chapter01/section02/thread_1_2_4/pro_1_sameNum/MyThread.java
@@ -12,7 +12,8 @@ public class MyThread extends Thread{
     private int i = 5;
 
     @Override
-    public void run() {
+    //public void run() {
+    synchronized public void run() {
         System.out.println("i=" + (i--) + " threadName="
                 + Thread.currentThread().getName());
     }
