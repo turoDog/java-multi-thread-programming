aff59d928eeb3f601f1bc41e6735dd40a95059da
陈志远 <chenzy01@eastcom-sw.com>
2019-11-12 15:45:56 +0800

    :white_check_mark: interrupt method can't stop the thread

---
+++ src/main/java/chapter01/section07/thread_1_7_1/pro_1_t11/MyThread.java
@@ -0,0 +1,20 @@
+package chapter01.section07.thread_1_7_1.pro_1_t11;
+
+/**
+ * Project Name:java-multi-thread-programming <br/>
+ * Package Name:chapter01.section07.thread_1_7_1.pro_1_t11 <br/>
+ * Date:2019/11/12 15:41 <br/>
+ *
+ * @author <a href="mailto:chenzy01@eastcom-sw.com">chenzy</a><br/>
+ */
+public class MyThread extends Thread{
+
+    @Override
+    public void run() {
+        super.run();
+        for (int i = 0; i < 500000; i++) {
+            System.out.println("i=" + (i + 1));
+        }
+    }
+
+}
