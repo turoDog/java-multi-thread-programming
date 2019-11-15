24cdf878afd097d3df21269fc18679ab664d317c
陈志远 <chenzy01@eastcom-sw.com>
2019-11-15 17:36:17 +0800

    :white_check_mark: anyString 是同一个对象，对于两个线程来说就是同一把锁，所以同步执行.

--- src/main/java/chapter02/section02/thread_2_2_7/pro_1_syncBlockString/Service.java
+++ src/main/java/chapter02/section02/thread_2_2_7/pro_1_syncBlockString/Service.java
@@ -11,10 +11,9 @@ public class Service {
 
     private String usernameParam;
     private String passwordParam;
-
+    private String anyString = new String();
     public void setUsernamePassword(String username, String password) {
         try {
-            String anyString = new String();
             synchronized (anyString) {
                 System.out.println("线程名称为：" + Thread.currentThread().getName()
                         + "在" + System.currentTimeMillis() + "进入同步块");
