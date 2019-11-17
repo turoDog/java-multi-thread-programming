547c00160c2936af20d119ad99b1de881d05dc86
turoDog <turodog@foxmail.com>
2019-11-17 23:35:41 +0800

    :sparkles: 使用 synchronized 同步代码块解决死循环问题

--- src/main/java/chapter02/section02/thread_2_2_11/pro_1_twoStop/Service.java
+++ src/main/java/chapter02/section02/thread_2_2_11/pro_1_twoStop/Service.java
@@ -9,21 +9,23 @@ package chapter02.section02.thread_2_2_11.pro_1_twoStop;
  */
 public class Service {
 
-    synchronized public void methodA() {
-
-        System.out.println("methodA begin");
-        boolean isContinueRun = true;
-        while (isContinueRun) {
+    public void methodA() {
+        Object object1 = new Object();
+        synchronized (object1) {
+            System.out.println("methodA begin");
+            boolean isContinueRun = true;
+            while (isContinueRun) {
+            }
+            System.out.println("methodA end");
         }
-        System.out.println("methodA end");
-
     }
 
-    synchronized public void methodB() {
-
-        System.out.println("methodB begin");
-        System.out.println("methodB end");
-
+    public void methodB() {
+        Object object2 = new Object();
+        synchronized (object2) {
+            System.out.println("methodB begin");
+            System.out.println("methodB end");
+        }
     }
 
 }
