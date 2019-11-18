e8745e2126df0cc0d085cbebb19b68c2fabec429
turoDog <turodog@foxmail.com>
2019-11-18 23:47:32 +0800

    :sparkles: 主线程直接启动 b 线程，此时 a、b 线程争抢的锁是 "123",所以同步执行

---
+++ src/main/java/chapter02/section02/thread_2_2_16/pro_1_setNewStringTwoLock/Run2.java
@@ -0,0 +1,26 @@
+package chapter02.section02.thread_2_2_16.pro_1_setNewStringTwoLock;
+
+/**
+ * Project Name:java-multi-thread-programming <br/>
+ * Package Name:chapter02.section02.thread_2_2_16.pro_1_setNewStringTwoLock <br/>
+ * Date:2019/11/18 23:17 <br/>
+ *
+ * @author <a href="turodog@foxmail.com">chenzy</a><br/>
+ */
+public class Run2 {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        MyService service = new MyService();
+
+        ThreadA a = new ThreadA(service);
+        a.setName("A");
+
+        ThreadB b = new ThreadB(service);
+        b.setName("B");
+
+        a.start();
+        b.start();
+    }
+
+}
