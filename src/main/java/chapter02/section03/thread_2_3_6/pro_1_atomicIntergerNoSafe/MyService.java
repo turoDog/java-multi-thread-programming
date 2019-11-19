22e0b47b208ffd2f5dad8121d1dd9a3c8db63a7b
turoDog <turodog@foxmail.com>
2019-11-19 23:03:19 +0800

    :sparkles: addAndGet 方法是原子的，但方法与方法间的调用是非原子的，解决方法是加入 synchronized 修饰调用的方法，解决非线程安全问题

--- src/main/java/chapter02/section03/thread_2_3_6/pro_1_atomicIntergerNoSafe/MyService.java
+++ src/main/java/chapter02/section03/thread_2_3_6/pro_1_atomicIntergerNoSafe/MyService.java
@@ -13,7 +13,7 @@ public class MyService {
 
     public static AtomicLong aiRef = new AtomicLong();
 
-    public void addNum() {
+    synchronized public void addNum() {
         System.out.println(Thread.currentThread().getName() + "加了100之后的值是:"
                 + aiRef.addAndGet(100));
         aiRef.addAndGet(1);
