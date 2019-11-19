cd677711f4d2b9ac7be58a857bd7e31d94ea21d2
turoDog <turodog@foxmail.com>
2019-11-19 22:48:01 +0800

    :sparkles: 用 synchronized 修饰 addCount 方法，解决 volatile 变量非原子性不同步的问题

--- src/main/java/chapter02/section03/thread_2_3_4/pro_1_volatileTestThread/MyThread.java
+++ src/main/java/chapter02/section03/thread_2_3_4/pro_1_volatileTestThread/MyThread.java
@@ -11,7 +11,7 @@ public class MyThread extends Thread{
 
     volatile public static int count;
 
-    private static void addCount() {
+    synchronized private static void addCount() {
         for (int i = 0; i < 100; i++) {
             count++;
         }
