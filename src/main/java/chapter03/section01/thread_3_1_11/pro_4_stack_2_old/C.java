6332851fb2b26cbc70563452ca4757e63ef4dc31
turoDog <turodog@foxmail.com>
2019-11-23 15:12:55 +0800

    :sparkles: 一生产与多消费，操作栈：解决 wait 条件改变与假死

---
+++ src/main/java/chapter03/section01/thread_3_1_11/pro_4_stack_2_old/C.java
@@ -0,0 +1,23 @@
+package chapter03.section01.thread_3_1_11.pro_4_stack_2_old;
+
+/**
+ * Project Name:java-multi-thread-programming <br/>
+ * Package Name:chapter03.section01.thread_3_1_11.pro_4_stack_2_old <br/>
+ * Date:2019/11/23 15:06 <br/>
+ *
+ * @author <a href="turodog@foxmail.com">chenzy</a><br/>
+ */
+public class C {
+
+    private MyStack myStack;
+
+    public C(MyStack myStack) {
+        super();
+        this.myStack = myStack;
+    }
+
+    public void popService() {
+        System.out.println("pop=" + myStack.pop());
+    }
+
+}
---
+++ src/main/java/chapter03/section01/thread_3_1_11/pro_4_stack_2_old/Run.java
@@ -0,0 +1,38 @@
+package chapter03.section01.thread_3_1_11.pro_4_stack_2_old;
+
+/**
+ * Project Name:java-multi-thread-programming <br/>
+ * Package Name:chapter03.section01.thread_3_1_11.pro_4_stack_2_old <br/>
+ * Date:2019/11/23 15:07 <br/>
+ *
+ * @author <a href="turodog@foxmail.com">chenzy</a><br/>
+ */
+public class Run {
+
+    public static void main(String[] args) throws InterruptedException {
+        MyStack myStack = new MyStack();
+
+        P p = new P(myStack);
+
+        C r1 = new C(myStack);
+        C r2 = new C(myStack);
+        C r3 = new C(myStack);
+        C r4 = new C(myStack);
+        C r5 = new C(myStack);
+
+        P_Thread pThread = new P_Thread(p);
+        pThread.start();
+
+        C_Thread cThread1 = new C_Thread(r1);
+        C_Thread cThread2 = new C_Thread(r2);
+        C_Thread cThread3 = new C_Thread(r3);
+        C_Thread cThread4 = new C_Thread(r4);
+        C_Thread cThread5 = new C_Thread(r5);
+        cThread1.start();
+        cThread2.start();
+        cThread3.start();
+        cThread4.start();
+        cThread5.start();
+    }
+
+}
