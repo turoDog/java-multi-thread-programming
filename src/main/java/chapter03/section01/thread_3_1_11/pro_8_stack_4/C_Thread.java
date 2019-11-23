a3388be734d4d0f7a0884067fdbacf1c5c60a227
turoDog <turodog@foxmail.com>
2019-11-23 15:33:30 +0800

    :sparkles: 多生产与多消费，操作栈

---
+++ src/main/java/chapter03/section01/thread_3_1_11/pro_8_stack_4/C_Thread.java
@@ -0,0 +1,26 @@
+package chapter03.section01.thread_3_1_11.pro_8_stack_4;
+
+/**
+ * Project Name:java-multi-thread-programming <br/>
+ * Package Name:chapter03.section01.thread_3_1_11.pro_4_stack_2_old <br/>
+ * Date:2019/11/23 15:06 <br/>
+ *
+ * @author <a href="turodog@foxmail.com">chenzy</a><br/>
+ */
+public class C_Thread extends Thread{
+
+    private C r;
+
+    public C_Thread(C r) {
+        super();
+        this.r = r;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            r.popService();
+        }
+    }
+
+}
---
+++ src/main/java/chapter03/section01/thread_3_1_11/pro_8_stack_4/MyStack.java
@@ -0,0 +1,48 @@
+package chapter03.section01.thread_3_1_11.pro_8_stack_4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Project Name:java-multi-thread-programming <br/>
+ * Package Name:chapter03.section01.thread_3_1_11.pro_4_stack_2_old <br/>
+ * Date:2019/11/23 15:06 <br/>
+ *
+ * @author <a href="turodog@foxmail.com">chenzy</a><br/>
+ */
+public class MyStack {
+
+    private List list = new ArrayList();
+
+    synchronized public void push() {
+        try {
+            while (list.size() == 1) {
+                this.wait();
+            }
+            list.add("anyString=" + Math.random());
+            this.notifyAll();
+            System.out.println("push=" + list.size());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    synchronized public String pop() {
+        String returnValue = "";
+        try {
+            while (list.size() == 0) {
+                System.out.println("pop操作中的："
+                        + Thread.currentThread().getName() + " 线程呈wait状态");
+                this.wait();
+            }
+            returnValue = "" + list.get(0);
+            list.remove(0);
+            this.notifyAll();
+            System.out.println("pop=" + list.size());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return returnValue;
+    }
+
+}
