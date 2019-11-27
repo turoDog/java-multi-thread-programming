5901bf5706c56b7bfc0cd6b9459d3b4416eaf5a6
turoDog <turodog@foxmail.com>
2019-11-27 15:09:14 +0800

    :sparkles: ReentrantLock hasQueuedThread(threadA)查询指定的线程是否正在等待获取此锁定 hasQueuedThreads() 查询是否有线程正在等待获取此锁定

---
+++ src/main/java/chapter04/thread_4_1_11/pro_1_lockMethodTest2/test1/Run.java
@@ -0,0 +1,31 @@
+package chapter04.thread_4_1_11.pro_1_lockMethodTest2.test1;
+
+/**
+ * Project Name:java-multi-thread-programming <br/>
+ * Package Name:chapter04.thread_4_1_11.pro_1_lockMethodTest2.test1 <br/>
+ * Date:2019/11/27 14:59 <br/>
+ *
+ * @author <a href="mailto:chenzy01@eastcom-sw.com">chenzy</a><br/>
+ */
+public class Run {
+
+    public static void main(String[] args) throws InterruptedException {
+        final Service service = new Service();
+
+        Runnable runnable = service::waitMethod;
+
+        Thread threadA = new Thread(runnable);
+        threadA.start();
+
+        Thread.sleep(500);
+
+        Thread threadB = new Thread(runnable);
+        threadB.start();
+
+        Thread.sleep(500);
+        System.out.println(service.lock.hasQueuedThread(threadA));
+        System.out.println(service.lock.hasQueuedThread(threadB));
+        System.out.println(service.lock.hasQueuedThreads());
+    }
+
+}
