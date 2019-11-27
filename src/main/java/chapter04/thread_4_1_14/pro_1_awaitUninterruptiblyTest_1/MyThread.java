14b3325274e6698abcc0b383abe100843880f2cb
turoDog <turodog@foxmail.com>
2019-11-27 16:25:29 +0800

    :sparkles: 程序运行出现异常，属正常现象

---
+++ src/main/java/chapter04/thread_4_1_14/pro_1_awaitUninterruptiblyTest_1/MyThread.java
@@ -0,0 +1,24 @@
+package chapter04.thread_4_1_14.pro_1_awaitUninterruptiblyTest_1;
+
+/**
+ * Project Name:java-multi-thread-programming <br/>
+ * Package Name:chapter04.thread_4_1_14.pro_1_awaitUninterruptiblyTest_1 <br/>
+ * Date:2019/11/27 16:08 <br/>
+ *
+ * @author <a href="mailto:chenzy01@eastcom-sw.com">chenzy</a><br/>
+ */
+public class MyThread extends Thread{
+
+    private Service service;
+
+    public MyThread(Service service) {
+        super();
+        this.service = service;
+    }
+
+    @Override
+    public void run() {
+        service.testMethod();
+    }
+
+}
