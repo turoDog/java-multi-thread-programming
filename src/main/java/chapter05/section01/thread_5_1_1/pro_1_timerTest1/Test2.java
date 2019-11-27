80f1a44742df517e0124af53c450c77a812625cf
turoDog <turodog@foxmail.com>
2019-11-27 17:50:12 +0800

    :sparkles: 执行任务的时间晚于当前时间，将 timer 新创建的线程改为守护线程

---
+++ src/main/java/chapter05/section01/thread_5_1_1/pro_1_timerTest1/Test2.java
@@ -0,0 +1,29 @@
+package chapter05.section01.thread_5_1_1.pro_1_timerTest1;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Timer;
+
+/**
+ * Project Name:java-multi-thread-programming <br/>
+ * Package Name:chapter05.section01.thread_5_1_1.pro_1_timerTest1 <br/>
+ * Date:2019/11/27 17:39 <br/>
+ *
+ * @author <a href="mailto:chenzy01@eastcom-sw.com">chenzy</a><br/>
+ */
+public class Test2 {
+
+    public static void main(String[] args) {
+        System.out.println("当前时间为："+new Date());
+        Calendar calendarRef = Calendar.getInstance();
+        calendarRef.add(Calendar.SECOND, 10);
+        Date runDate = calendarRef.getTime();
+
+        MyTask task = new MyTask();
+        //守护线程
+        Timer timer = new Timer(true);
+        timer.schedule(task, runDate);
+
+    }
+
+}
