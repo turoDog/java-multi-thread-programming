58506c3ed4d9f2c2044076068f42192ff0071bf0
turoDog <turodog@foxmail.com>
2019-11-27 16:58:16 +0800

    :sparkles: 利用 Condition 对象，让线程按顺序执行

---
+++ src/main/java/chapter04/thread_4_1_16/pro_1_condition123/F.java
@@ -0,0 +1,14 @@
+package chapter04.thread_4_1_16.pro_1_condition123;
+
+/**
+ * Project Name:java-multi-thread-programming <br/>
+ * Package Name:chapter04.thread_4_1_16.pro_1_condition123 <br/>
+ * Date:2019/11/27 16:53 <br/>
+ *
+ * @author <a href="mailto:chenzy01@eastcom-sw.com">chenzy</a><br/>
+ */
+public class F {
+
+    volatile public static int nextPrintWho = 1;
+
+}
