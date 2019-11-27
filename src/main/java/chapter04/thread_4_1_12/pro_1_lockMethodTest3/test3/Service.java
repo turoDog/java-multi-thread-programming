83f42199e9e358bbfe97641e353dc991cfb1f8d0
turoDog <turodog@foxmail.com>
2019-11-27 15:30:34 +0800

    :sparkles: ReentrantLock isLocked() 查询此锁定是否由任意线程保持

---
+++ src/main/java/chapter04/thread_4_1_12/pro_1_lockMethodTest3/test3/Service.java
@@ -0,0 +1,31 @@
+package chapter04.thread_4_1_12.pro_1_lockMethodTest3.test3;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * Project Name:java-multi-thread-programming <br/>
+ * Package Name:chapter04.thread_4_1_12.pro_1_lockMethodTest3.test1 <br/>
+ * Date:2019/11/27 15:19 <br/>
+ *
+ * @author <a href="mailto:chenzy01@eastcom-sw.com">chenzy</a><br/>
+ */
+public class Service {
+
+    private ReentrantLock lock;
+
+    public Service(boolean isFair) {
+        super();
+        lock = new ReentrantLock(isFair);
+    }
+
+    public void serviceMethod() {
+        try {
+            System.out.println(lock.isLocked());
+            lock.lock();
+            System.out.println(lock.isLocked());
+        } finally {
+            lock.unlock();
+        }
+    }
+
+}
