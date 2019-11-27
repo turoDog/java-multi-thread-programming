00918ca8850f7f3800ea43b5dc315d8640c5e83a
turoDog <turodog@foxmail.com>
2019-11-27 15:27:45 +0800

    :sparkles: ReentrantLock isHeldByCurrentThread() 查询当前线程是否保持此锁定

---
+++ src/main/java/chapter04/thread_4_1_12/pro_1_lockMethodTest3/test2/Run.java
@@ -0,0 +1,19 @@
+package chapter04.thread_4_1_12.pro_1_lockMethodTest3.test2;
+
+/**
+ * Project Name:java-multi-thread-programming <br/>
+ * Package Name:chapter04.thread_4_1_12.pro_1_lockMethodTest3.test1 <br/>
+ * Date:2019/11/27 15:19 <br/>
+ *
+ * @author <a href="mailto:chenzy01@eastcom-sw.com">chenzy</a><br/>
+ */
+public class Run {
+
+    public static void main(String[] args) throws InterruptedException {
+        final Service service1 = new Service(true);
+        Runnable runnable = service1::serviceMethod;
+        Thread thread = new Thread(runnable);
+        thread.start();
+    }
+
+}
